c3e7da1fb20e62ba6e655495e9f2c9d54c985896
Max <maxnelson997@gmail.com>
2017-11-21 17:03:15 -0700

    added plus and minus

--- College Weighted Average Grade Calculator/ViewController.swift
+++ College Weighted Average Grade Calculator/ViewController.swift
@@ -15,19 +15,31 @@ class MainController: UIViewController {
     
     var percentView:TitleView = {
         let m = TitleView(title: "Percent")
-        m.layer.cornerRadius = 6
+        m.layer.cornerRadius = 12
         return m
     }()
     
     var gradeView:TitleView = {
         let m = TitleView(title: "Grade")
-        m.layer.cornerRadius = 6
+        m.layer.cornerRadius = 12
         return m
     }()
     
     var catsContainer:TitleView = {
-        let m = TitleView(title: "Class Categories")
-        m.layer.cornerRadius = 12
+        let m = TitleView(title: "Class Categories", perfectSquare: false)
+        m.layer.cornerRadius = 20
+        return m
+    }()
+    
+    var plusButton:TitleInView = {
+        let m = TitleInView(title: "+")
+        m.layer.cornerRadius = 20
+        return m
+    }()
+    
+    var minusButton:TitleInView = {
+        let m = TitleInView(title: "-")
+        m.layer.cornerRadius = 20
         return m
     }()
     
@@ -35,6 +47,7 @@ class MainController: UIViewController {
         let s = UIStackView()
         s.translatesAutoresizingMaskIntoConstraints = false
         s.axis = .vertical
+        s.spacing = 10
         return s
     }()
     
@@ -42,8 +55,13 @@ class MainController: UIViewController {
         let s = UIStackView()
         s.translatesAutoresizingMaskIntoConstraints = false
         s.axis = UILayoutConstraintAxis.horizontal
-
-        
+        return s
+    }()
+    
+    var controlStack:UIStackView = {
+        let s = UIStackView()
+        s.translatesAutoresizingMaskIntoConstraints = false
+        s.axis = .horizontal
         return s
     }()
     
@@ -60,27 +78,34 @@ class MainController: UIViewController {
 //        topStack.addArrangedSubview(UIView())
         topStack.addArrangedSubview(gradeView)
         
-        let spacer0 = UIView()
-        spacer0.translatesAutoresizingMaskIntoConstraints = false
+        controlStack.addArrangedSubview(minusButton)
+        controlStack.addArrangedSubview(plusButton)
+        
+//        let spacer0 = UIView()
+//        spacer0.translatesAutoresizingMaskIntoConstraints = false
         let spacer1 = UIView()
         spacer1.translatesAutoresizingMaskIntoConstraints = false
         mainStack.addArrangedSubview(topStack)
-        mainStack.addArrangedSubview(spacer0)
+ 
         mainStack.addArrangedSubview(catsContainer)
+        mainStack.addArrangedSubview(controlStack)
         mainStack.addArrangedSubview(spacer1)
         
+        
         NSLayoutConstraint.activate([
-           
+            minusButton.widthAnchor.constraint(equalTo: percentView.widthAnchor),
+            plusButton.widthAnchor.constraint(equalTo: percentView.widthAnchor),
+            plusButton.heightAnchor.constraint(equalToConstant: 50),
+            minusButton.heightAnchor.constraint(equalToConstant: 50),
             percentView.widthAnchor.constraint(equalTo: mainStack.widthAnchor, multiplier: 0.5),
             percentView.heightAnchor.constraint(equalTo: percentView.widthAnchor),
             gradeView.widthAnchor.constraint(equalTo: percentView.widthAnchor),
             gradeView.heightAnchor.constraint(equalTo: percentView.heightAnchor),
-            spacer0.heightAnchor.constraint(equalToConstant: 30),
+     
             spacer1.heightAnchor.constraint(equalToConstant: 50),
             topStack.heightAnchor.constraint(equalTo: percentView.widthAnchor),
-            catsContainer.heightAnchor.constraint(lessThanOrEqualTo: mainStack.heightAnchor, multiplier: 1)
-        
-            
+            catsContainer.heightAnchor.constraint(lessThanOrEqualTo: mainStack.heightAnchor, multiplier: 1),
+            catsContainer.widthAnchor.constraint(equalTo: mainStack.widthAnchor, multiplier: 1)
             ])
         //stackview
         

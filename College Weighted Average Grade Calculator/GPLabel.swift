15b99855e9003e8228bc15ae985e4de6f04db429
Max <maxnelson997@gmail.com>
2017-11-21 17:55:50 -0700

    changes to gplabel

--- College Weighted Average Grade Calculator/GPLabel.swift
+++ College Weighted Average Grade Calculator/GPLabel.swift
@@ -24,6 +24,8 @@ class GPLabel: UILabel {
         phaseTwo()
     }
     
+    //hmm my local changes
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
